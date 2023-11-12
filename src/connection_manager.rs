c3bf393f6d9f9b3a0a0a16e3a44334922929bdd0
splashling <splashling1789@gmail.com>
2023-11-12 16:17:57 +0100

    Project structure separated

---
+++ src/connection_manager.rs
@@ -0,0 +1,40 @@
+use sqlx::{Connection, Error, query, SqliteConnection};
+use sqlx::sqlite::SqliteQueryResult;
+
+const SQLITE_FILENAME:&str = "db.sqlite";
+const QUERY_INITIALIZE_TABLES:&str = "CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, description TEXT NOT NULL, done INTEGER DEFAULT 0);";
+
+async fn manage_connection() -> Option<SqliteConnection> {
+    println!("Connecting to {SQLITE_FILENAME}");
+    match SqliteConnection::connect(SQLITE_FILENAME).await {
+        Ok(conn) => {
+            Some(conn)
+        }
+        Err(_) => {
+            None
+        }
+    }
+}
+
+async fn initialize_db(conn: &mut SqliteConnection) -> Result<SqliteQueryResult, Error> {
+    query(QUERY_INITIALIZE_TABLES).execute(conn).await
+}
+
+async fn db_connect() -> SqliteConnection {
+    rusqlite::Connection::open(SQLITE_FILENAME).expect("Failed to create database");
+    manage_connection().await.expect("An error ocurred connecting to the database")
+}
+
+pub async fn create_connection() -> Option<SqliteConnection>{
+    let mut conn = db_connect().await;
+    match initialize_db(&mut conn).await {
+        Ok(_) => {
+            println!("Connected correctly!");
+            Some(conn)
+        }
+        Err(e) => {
+            println!("Error: {e}");
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+mod connection_manager;
+mod menu;
+
 use sqlx::{Connection, Error, query, Row, SqliteConnection};
 use sqlx::sqlite::SqliteQueryResult;
 use std::io::stdin;
 use clearscreen::clear;
+use crate::connection_manager::create_connection;
 
-const SQLITE_FILENAME:&str = "db.sqlite";
-const QUERY_INITIALIZE_TABLES:&str = "CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, description TEXT NOT NULL, done INTEGER DEFAULT 0);";
+
 const QUERY_GET_TODOS:&str = "SELECT * FROM tasks";
 const QUERY_ALTER_TODO:&str = "UPDATE tasks SET done = CASE WHEN done = 0 THEN 1 ELSE 0 END WHERE id = ?;";
 const QUERY_DELETE_WHERE_ID : &str = "DELETE FROM tasks WHERE id= ?";
@@ -20,78 +23,60 @@ struct ToDo {
 #[tokio::main]
 async fn main() {
     println!("Welcome to the Terminal-ToDo!");
-    println!("Connecting to {SQLITE_FILENAME}");
-    rusqlite::Connection::open(SQLITE_FILENAME).expect("Failed to create database");
-    let mut conn = manage_connection().await.expect("An error ocurred connecting to the database");
-    match initialize_db(&mut conn).await {
-        Ok(_) => {
-            println!("Connected correctly!");
-        }
-        Err(e) => {
-            println!("Error: {e}");
-        }
-    }
-    let mut show_done = false;
-    loop {
-        clear().expect("Failed to clear screen");
-        let list = get_todos(&mut conn).await.expect("Failed to obtain db content from table tasks");
-        println!("Terminal-Todo\n");
-        print_todos(list, show_done);
-        println!("- Type a description to create a new task.");
-        println!("- Type the task id you want to mark as done or undone.");
-        println!("- Type \"/done\" to alternate showing or not showing done tasks.");
-        println!("- Type \"/delete\" to delete a task with a given id.");
-        let mut input = String::default();
+    match create_connection().await {
+        Some(mut conn) => {
+            let mut show_done = false;
+            loop {
+                clear().expect("Failed to clear screen");
+                let list = get_todos(&mut conn).await.expect("Failed to obtain db content from table tasks");
+                println!("Terminal-Todo\n");
+                print_todos(list, show_done);
+                println!("- Type a description to create a new task.");
+                println!("- Type the task id you want to mark as done or undone.");
+                println!("- Type \"/done\" to alternate showing or not showing done tasks.");
+                println!("- Type \"/delete\" to delete a task with a given id.");
+                let mut input = String::default();
 
-        stdin().read_line(&mut input).expect("Failed to get stdin");
-        let content= input.trim();
-        match input.trim().parse::<i32>() {
-            Ok(id) => {
-                query(QUERY_ALTER_TODO).bind(id).execute(&mut conn).await.expect("Error executing the query");
-            }
-            Err(_) => {
-                if content.starts_with("/") {
-                    match content {
-                        "/done" => { show_done = !show_done; }
-                        "/delete" => {
-                            input = String::default();
-                            println!("Input the id or description of the task you want to delete: ");
-                            stdin().read_line(&mut input).expect("Failed to get stdin");
-                            match input.trim().parse::<i32>() {
-                                Ok(id) => {
-                                    query(QUERY_DELETE_WHERE_ID).bind(id).execute(&mut conn).await.expect("Failed to execute query");
+                stdin().read_line(&mut input).expect("Failed to get stdin");
+                let content= input.trim();
+                match input.trim().parse::<i32>() {
+                    Ok(id) => {
+                        query(QUERY_ALTER_TODO).bind(id).execute(&mut conn).await.expect("Error executing the query");
+                    }
+                    Err(_) => {
+                        if content.starts_with("/") {
+                            match content {
+                                "/done" => { show_done = !show_done; }
+                                "/delete" => {
+                                    input = String::default();
+                                    println!("Input the id or description of the task you want to delete: ");
+                                    stdin().read_line(&mut input).expect("Failed to get stdin");
+                                    match input.trim().parse::<i32>() {
+                                        Ok(id) => {
+                                            query(QUERY_DELETE_WHERE_ID).bind(id).execute(&mut conn).await.expect("Failed to execute query");
+                                        }
+                                        Err(_) => {
+                                            query(QUERY_DELETE_WHERE_DESC).bind(input.trim()).execute(&mut conn).await.expect("Failed to execute query");
+                                        }
+                                    }
                                 }
-                                Err(_) => {
-                                    query(QUERY_DELETE_WHERE_DESC).bind(input.trim()).execute(&mut conn).await.expect("Failed to execute query");
-                                }
+                                _ => {}
                             }
                         }
-                        _ => {}
+                        else {
+                            query(QUERY_INSERT_VALUE).bind(content).execute(&mut conn).await.expect("Failed to execute query");
+                        }
                     }
-                }
-                else {
-                    query(QUERY_INSERT_VALUE).bind(content).execute(&mut conn).await.expect("Failed to execute query");
                 }
             }
         }
-    }
-
-}
-
-async fn manage_connection() -> Option<SqliteConnection> {
-    match SqliteConnection::connect(SQLITE_FILENAME).await {
-        Ok(conn) => {
-            Some(conn)
-        }
-        Err(_) => {
-            None
+        None => {
+            println!("Failed to create the database connection.");
         }
     }
 }
 
-async fn initialize_db(conn: &mut SqliteConnection) -> Result<SqliteQueryResult, Error> {
-    query(QUERY_INITIALIZE_TABLES).execute(conn).await
-}
+
 
 async fn get_todos(conn: &mut SqliteConnection) -> Option<Vec<ToDo>> {
     match query(QUERY_GET_TODOS).fetch_all(conn).await {
